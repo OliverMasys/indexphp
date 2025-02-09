1d73a5da663d08a15bbb42545aef5d4e9cbb299e
OliverMasys <noriaoliver@gmail.com>
2025-02-08 20:46:43 -0600

    First
    html only?

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HomePage</title>
+</head>
+
+<body>
+    <h1>Welcome to My Homepage</h1>
+    <h2>About Me</h2>
+    <p> My name is Oli, and I'm currently taking a web development class.
+            I enjoy Learning about code, playing video games, reading manga, comics and hanging out with friends.
+            Am interested in many things! Learning to mod my favorite games, excreting my body(doctor orders) and try to do Youtube.
+            I hope to learn and understand more on web programming hopefully after taking class and practice on my own I can grow
+            my skills and make better websites of my own.</p>
+    <h2>Projects and Goals</h2>
+        <ul>
+            <li>No Projects at the moment</li>
+            <li>Learn and Understand PHP as one of my Goals</li>
+        </ul>
+</body>
+
+</html>
